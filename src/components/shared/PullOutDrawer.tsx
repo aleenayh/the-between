572da14eb24db43a56823d39f1c2b0a2f5aa6af9
aleenayh <145587682+aleenayh@users.noreply.github.com>
2026-03-01 10:16:35 -0800

    padding tray mobile

--- src/components/shared/PullOutDrawer.tsx
+++ src/components/shared/PullOutDrawer.tsx
@@ -18,7 +18,7 @@ export function PullOutDrawer({ isOpen, setIsOpen, children }: { isOpen: boolean
       animate={{ left: 0, y: 0, opacity: 1 }}
       exit={{ left: transitionLeft, y: transitionTop, opacity: transitionOpacity }}
       transition={{ duration: prefersReducedMotion ? 0 : mobileScreenSize ? 0.5 : 1, ease: "linear", }}
-      className="absolute top-0 left-0 w-full pb-8 md:pb-4 md:w-1/2 h-screen flex flex-col justify-start items-center bg-theme-bg-primary border-r border-theme-border-accent rounded-lg p-4 z-10 overflow-y-auto pointer-events-auto"
+      className="absolute top-0 left-0 w-full pb-10 md:pb-4 md:w-1/2 h-screen flex flex-col justify-start items-center bg-theme-bg-primary border-r border-theme-border-accent rounded-lg p-4 z-10 overflow-y-auto pointer-events-auto"
     >
       <div className="hidden md:block"><CloseButton onClick={() => setIsOpen(false)} /></div>
       {children}
