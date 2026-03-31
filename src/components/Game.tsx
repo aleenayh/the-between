54a3dda0b008ff409d90715ff66ea53029058073
aleenayh <145587682+aleenayh@users.noreply.github.com>
2026-03-31 08:42:43 -0700

    bottom nav bar for mobile sticky

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -5,7 +5,7 @@ import { CharacterOverview } from "./playbooks/CharacterOverview";
 
 export function Game() {
 	return (
-		<div className="flex flex-col w-full h-full p-4">
+		<div className="flex flex-col w-full h-full pt-2 p-1 pb-0 md:p-4">
 		<Drawers />
 		<div className="ml-0 md:ml-8 flex-1 min-h-0 overflow-hidden z-[5]">
 			<CharacterOverview />
